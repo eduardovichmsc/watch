4a9820d95f52accfb1ec61a134c99e554749dd79
eduardovichmsc <eduardovdarzhan@gmail.com>
2025-09-29 04:29:01 +0500

    added: toploader on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 import { CustomCursor } from "@/components/ui/cursor";
 import { Preloader } from "@/components/layouts/preloader";
@@ -25,6 +26,17 @@ export default function RootLayout({
 		<html lang="en">
 			<body
 				className={`${ibmPlexMono.variable} ${urbanist.variable} ${geistSans.variable} ${cormorant.variable} ${geistMono.variable} antialiased`}>
+				<NextTopLoader
+					color="#000000"
+					initialPosition={0.5}
+					crawlSpeed={50}
+					height={5}
+					crawl={false}
+					showSpinner={false}
+					easing="ease"
+					speed={50}
+					shadow="0 0 10px #000000,0 0 5px #000000"
+				/>
 				<Preloader />
 
 				<CustomCursor />
